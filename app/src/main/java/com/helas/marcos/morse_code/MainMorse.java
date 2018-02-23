6182c7a3a737c69f81314fb2b05bfe0179cec60a
eleves <eleves@MarcosPaulo.local>
2018-02-23 10:19:30 -0500

    Initial commit

---
+++ app/src/main/java/com/helas/marcos/morse_code/MainMorse.java
@@ -0,0 +1,15 @@
+package com.helas.marcos.morse_code;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMorse extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_morse);
+    }
+}
